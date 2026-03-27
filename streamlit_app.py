c093c0e55388039d933585cb31111d649c005960
Deo209 <deonisiamendoncadejesussynizya@gmail.com>
2026-03-27 02:30:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Python")
+st.title("Aply :blue[Python :cool:")
 st.write(
     "let's start build! for help inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)"
 )
