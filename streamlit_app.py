65968b774723a8d5c989726af2c6a25cc564615d
Deo209 <deonisiamendoncadejesussynizya@gmail.com>
2026-03-27 02:35:04 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("[Python] :rocket:")
+st.title("lety")
 st.write(
     "let's start build! for help inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)"
 )
